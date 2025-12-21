0bd120707ff4567f9bc3e017da253b87d3b4b9b9
Lasty120 <100523859+Lasty120@users.noreply.github.com>
2025-12-21 14:25:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const themeBtn = document.getElementById("themeBtn");
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+let count = 0;
+const countBtn = document.getElementById("countBtn");
+const counter = document.getElementById("counter");
+
+countBtn.addEventListener("click", () => {
+    count++;
+    counter.textContent = count;
+});
+
+const toggleBtn = document.getElementById("toggleBtn");
+const hiddenText = document.getElementById("hiddenText");
+
+toggleBtn.addEventListener("click", () => {
+    hiddenText.classList.toggle("hidden");
+});
 
